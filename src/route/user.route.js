1e93d7d78f5f773d0a0c13813a5d5eadafc05614
Tanvir Islam Streame <engr.tanvir.islam.1@gmail.com>
2022-10-06 21:26:41 +0600

    Remove node_modules

--- src/route/user.route.js
+++ src/route/user.route.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const router = express.Router();
-const userController = require("../user/user.conreoller")
+const userController = require("../user/user.controller")
 router.get("/", userController.userList);
 module.exports = router
---
+++ src/user/user.controller.js
@@ -0,0 +1,32 @@
+const { userData } = require("../data/users");
+exports.userList = async (req, res) => {
+    if(req.query?.age_lt && req.query?.age_gt) {
+        const result = userData.filter(eachAge => eachAge.age <= req.query?.age_lt && eachAge.age >= req.query?.age_gt );
+        return res.json(result);
+    }
+
+    else if(req.query?.age_lt) {
+        const result = userData.filter(eachAge => eachAge.age <= req.query?.age_lt);
+        return res.json(result);
+    }
+
+    else if(req.query?.age_gt) {
+        const result = userData.filter(eachAge => eachAge.age >= req.query?.age_gt);
+        return res.json(result);
+    }
+
+    else if(req.query?.req.query?.name) {
+        const result = userData.filter(eachName => eachName.name.toLocaleLowerCase() == req.query?.name?.toLocaleLowerCase());
+        return res.json(result);
+    }
+
+    else {
+        return res.json(userData);
+    }
+}
+
+exports.userDetail = async (req, res) => {
+    console.log(req.params.id);
+    const data = userData.find(eachItem => eachItem.id == req.params.id)
+    return res.json(data);
+}
